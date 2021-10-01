28f2cf15a26fd5fcc4e8f3bee00648e681d4b59e
Chao Qin <hackerqc@gmail.com>
2021-10-02 03:31:40 +0800

    Add hardhat linter and github action.

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,7 @@ require("@nomiclabs/hardhat-etherscan");
 require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
+require("@nomiclabs/hardhat-solhint");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
