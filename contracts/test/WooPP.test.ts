f1aab17c4b12dc952250f35d9ea3cee8352cdd27
Chao Qin <hackerqc@gmail.com>
2021-10-05 06:50:30 +0800

    [Test] init the unit test config and sample test case.

--- contracts/test/WooPP.test.ts
+++ contracts/test/WooPP.test.ts
@@ -1,49 +1,29 @@
-import { expect, use } from 'chai'
-import { Contract } from 'ethers'
-import { deployContract, MockProvider, solidity } from 'ethereum-waffle'
-import BasicToken from '../build/BasicToken.json'
+const { expect } = require('chai')
+const { ethers } = require('hardhat')
 
-use(solidity)
+const { BN, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
 
-describe('BasicToken', () => {
-  const [wallet, walletTo] = new MockProvider().getWallets()
-  let token: Contract
+const WooPP = artifacts.require('WooPP');
 
-  beforeEach(async () => {
-    token = await deployContract(wallet, BasicToken, [1000])
-  })
+const [owner, addr1] = await ethers.getSigners();
 
-  it('Assigns initial balance', async () => {
-    expect(await token.balanceOf(wallet.address)).to.equal(1000)
-  })
+describe('WooPP Test Suite', function () {
+  before(async function () {
+    this.WooPP = await ethers.getContractFactory('WooPP');
+  });
 
-  it('Transfer adds amount to destination account', async () => {
-    await token.transfer(walletTo.address, 7)
-    expect(await token.balanceOf(walletTo.address)).to.equal(7)
-  })
+  beforeEach(async function () {
+    this.wooPP = await this.WooPP.deploy('', '', '', '');
+    await this.wooPP.deployed();
+  });
 
-  it('Transfer emits event', async () => {
-    await expect(token.transfer(walletTo.address, 7))
-      .to.emit(token, 'Transfer')
-      .withArgs(wallet.address, walletTo.address, 7)
-  })
+  // Test case
+  it('', async function () {
+    // Store a value
+    // await this.box.store(42);
 
-  it('Can not transfer above the amount', async () => {
-    await expect(token.transfer(walletTo.address, 1007)).to.be.reverted
-  })
-
-  it('Can not transfer from empty account', async () => {
-    const tokenFromOtherWallet = token.connect(walletTo)
-    await expect(tokenFromOtherWallet.transfer(wallet.address, 1)).to.be.reverted
-  })
-
-  it('Calls totalSupply on BasicToken contract', async () => {
-    await token.totalSupply()
-    expect('totalSupply').to.be.calledOnContract(token)
-  })
-
-  it('Calls balanceOf with sender address on BasicToken contract', async () => {
-    await token.balanceOf(wallet.address)
-    expect('balanceOf').to.be.calledOnContractWith(token, [wallet.address])
-  })
-})
+    // Test if the returned value is the same one
+    // Note that we need to use strings to compare the 256 bit integers
+    // expect((await this.box.retrieve()).toString()).to.equal('42');
+  });
+});
