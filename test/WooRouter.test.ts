266359fd61d0640880ba0ba8a101905e9ac24e10
merlincdj <377730858@qq.com>
2021-10-12 15:51:32 +0800

    [test]Add WooRouter test

--- test/WooRouter.test.ts
+++ test/WooRouter.test.ts
@@ -33,39 +33,160 @@
 
 import { expect, use } from 'chai'
 import { Contract } from 'ethers'
-import { deployContract, MockProvider, solidity } from 'ethereum-waffle'
+import {deployContract, deployMockContract, MockProvider, solidity} from 'ethereum-waffle'
 import Wooracle from '../build/Wooracle.json'
+import WooPP from '../build/WooPP.json'
+import WooRouter from '../build/WooRouter.json'
+import IERC20 from "../build/IERC20.json"
+import TestToken from "../build/TestToken.json";
 
 use(solidity)
 
 const ZERO_ADDR = '0x0000000000000000000000000000000000000000'
+const ZERO = 0
 
 describe('WooRouter', () => {
-  const [owner, user, quoteToken] = new MockProvider().getWallets()
+  const [owner, user] = new MockProvider().getWallets()
 
-  describe('#ctor and setters', () => {
+  describe('ctor, init & basic func', () => {
+    let wooracle: Contract
+    let wooPP: Contract
     let wooRouter: Contract
+    let baseToken: Contract
+    let quoteToken: Contract
 
-    beforeEach('deploy test oracle', async () => {
-      // wooRouter = await deployContract(owner, WooRouter, []);
+    before('Deploy ERC20', async () => {
+      baseToken = await deployContract(owner, TestToken, [])
+      quoteToken = await deployContract(owner, TestToken, [])
     })
 
-    // it('init', async () => {
-    //     expect(await wooracle._OWNER_()).to.eq(owner.address)
-    // })
+    beforeEach('Deploy WooRouter', async () => {
+      wooracle = await deployContract(owner, Wooracle, [])
+      wooPP = await deployContract(owner, WooPP, [quoteToken.address, wooracle.address, ZERO_ADDR])
+      wooRouter = await deployContract(owner, WooRouter, [wooPP.address])
+    })
 
-    // it('init fields', async () => {
-    //     expect(await wooracle.staleDuration()).to.eq(300)
-    //     expect(await wooracle.timestamp()).to.eq(0)
-    //     expect(await wooracle.quoteAddr()).to.eq(ZERO_ADDR)
-    // })
+    it('Init with correct owner', async () => {
+      expect(await wooRouter.owner()).to.eq(owner.address)
+    })
 
-    // it('setQuoteAddr', async () => {
-    //     expect(await wooracle.quoteAddr()).to.eq(ZERO_ADDR)
-    //     await wooracle.setQuoteAddr(quoteToken.address)
-    //     expect(await wooracle.quoteAddr()).to.eq(quoteToken.address)
-    // })
+    it('Init state variables', async () => {
+      expect(await wooRouter.quoteToken()).to.eq(quoteToken.address)
+      expect(await wooRouter.wooPool()).to.eq(wooPP.address)
+    })
+
+    it('setPool', async () => {
+      let anotherQuoteToken = await deployMockContract(owner, IERC20.abi)
+      let anotherWooPP = await deployContract(owner, WooPP, [anotherQuoteToken.address, wooracle.address, ZERO_ADDR])
+      await wooRouter.setPool(anotherWooPP.address)
+      expect(await wooRouter.quoteToken()).to.eq(anotherQuoteToken.address)
+      expect(await wooRouter.wooPool()).to.eq(anotherWooPP.address)
+    })
+
+    it('Emit WooPoolChanged when setPool', async () => {
+      let anotherQuoteToken = await deployMockContract(owner, IERC20.abi)
+      let anotherWooPP = await deployContract(owner, WooPP, [anotherQuoteToken.address, wooracle.address, ZERO_ADDR])
+      await wooRouter.setPool(anotherWooPP.address)
+      await expect(wooRouter.setPool(anotherWooPP.address)).to.emit(wooRouter, 'WooPoolChanged').withArgs(anotherWooPP.address)
+    })
+
+    it('Prevents non-owners from setPool', async () => {
+      let anotherQuoteToken = await deployMockContract(owner, IERC20.abi)
+      let anotherWooPP = await deployContract(owner, WooPP, [anotherQuoteToken.address, wooracle.address, ZERO_ADDR])
+      expect(wooRouter.connect(user).setPool(anotherWooPP.address)).to.be.revertedWith('Ownable: caller is not the owner')
+    })
+
+    it('setWhitelisted', async() => {
+      expect(await wooRouter.isWhitelisted(user.address)).to.eq(false)
+      await wooRouter.setWhitelisted(user.address, true)
+      expect(await wooRouter.isWhitelisted(user.address)).to.eq(true)
+      await wooRouter.setWhitelisted(user.address, false)
+      expect(await wooRouter.isWhitelisted(user.address)).to.eq(false)
+    })
+
+    it('Prevents zero addr from setWhitelisted', async() => {
+      expect(await wooRouter.isWhitelisted(ZERO_ADDR)).to.eq(false)
+      expect(wooRouter.setWhitelisted(ZERO_ADDR, true)).to.be.revertedWith('WooRouter: target_ADDR_ZERO')
+      expect(wooRouter.setWhitelisted(ZERO_ADDR, false)).to.be.revertedWith('WooRouter: target_ADDR_ZERO')
+    })
+
+    it('Prevents non-owners from setWhitelisted', async() => {
+      expect(await wooRouter.isWhitelisted(user.address)).to.eq(false)
+      expect(wooRouter.connect(user).setWhitelisted(user.address, true)).to.be.revertedWith('Ownable: caller is not the owner')
+      expect(wooRouter.connect(user).setWhitelisted(user.address, false)).to.be.revertedWith('Ownable: caller is not the owner')
+    })
+
+    it('rescueFunds', async() => {
+      expect(await baseToken.balanceOf(wooRouter.address)).to.eq(ZERO)
+      expect(await baseToken.balanceOf(owner.address)).to.eq(ZERO)
+
+      let mintBalance = 10000
+      await baseToken.mint(wooRouter.address, mintBalance)
+      expect(await baseToken.balanceOf(wooRouter.address)).to.eq(mintBalance)
+
+      await wooRouter.connect(owner).rescueFunds(baseToken.address, mintBalance)
+      expect(await baseToken.balanceOf(wooRouter.address)).to.eq(ZERO)
+      expect(await baseToken.balanceOf(owner.address)).to.eq(mintBalance)
+    })
+
+    it('Prevents zero addr as token addr from rescueFunds', async() => {
+      expect(wooRouter.rescueFunds(ZERO_ADDR, ZERO)).to.be.revertedWith('WooRouter: token_ADDR_ZERO')
+    })
+
+    it('Prevents non-owners from rescueFunds', async() => {
+      expect(await baseToken.balanceOf(wooRouter.address)).to.eq(ZERO)
+      expect(await baseToken.balanceOf(user.address)).to.eq(ZERO)
+
+      let mintBalance = 10000
+      await baseToken.mint(wooRouter.address, mintBalance)
+      expect(await baseToken.balanceOf(wooRouter.address)).to.eq(mintBalance)
+
+      expect(wooRouter.connect(user).rescueFunds(baseToken.address, mintBalance)).to.be.revertedWith('Ownable: caller is not the owner')
+      expect(await baseToken.balanceOf(user.address)).to.eq(ZERO)
+    })
+
+    it('destroy', async() => {
+      await wooRouter.destroy()
+      expect(wooRouter.quoteToken()).to.be.revertedWith('')
+    })
+
+    it('Prevents non-owners from destroy', async() => {
+      expect(wooRouter.connect(user).destroy()).to.be.revertedWith('Ownable: caller is not the owner')
+    })
   })
 
-  // TODO: add more test cases.
+  describe('core func', () => {
+    let wooracle: Contract
+    let wooPP: Contract
+    let wooRouter: Contract
+    let baseToken: Contract
+    let quoteToken: Contract
+
+    before('Deploy ERC20', async () => {
+      baseToken = await deployContract(owner, TestToken, [])
+      quoteToken = await deployContract(owner, TestToken, [])
+    })
+
+    beforeEach('Deploy WooRouter', async () => {
+      wooracle = await deployContract(owner, Wooracle, [])
+      wooPP = await deployContract(owner, WooPP, [quoteToken.address, wooracle.address, ZERO_ADDR])
+      wooRouter = await deployContract(owner, WooRouter, [wooPP.address])
+    })
+
+    it('sellBase', async () => {
+      // TODO waiting for WooPP.test.ts swap code
+      expect(await baseToken.balanceOf(user.address)).to.eq(ZERO)
+      let mintBaseAmount = 10000
+      await baseToken.mint(wooPP.address, mintBaseAmount)
+
+      let minQuoteAmount = 0
+      await wooRouter.sellBase(
+        baseToken.address,
+        mintBaseAmount,
+        minQuoteAmount,
+        user.address,
+        user.address
+      )
+    })
+  })
 })
