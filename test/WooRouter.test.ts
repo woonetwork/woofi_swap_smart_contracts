7a33964aa34caabedfb731a36551de08df71b571
Lint Action <lint-action@samuelmeuli.com>
2021-10-12 07:54:01 +0000

    Fix code style issues with Prettier

--- test/WooRouter.test.ts
+++ test/WooRouter.test.ts
@@ -33,12 +33,12 @@
 
 import { expect, use } from 'chai'
 import { Contract } from 'ethers'
-import {deployContract, deployMockContract, MockProvider, solidity} from 'ethereum-waffle'
+import { deployContract, deployMockContract, MockProvider, solidity } from 'ethereum-waffle'
 import Wooracle from '../build/Wooracle.json'
 import WooPP from '../build/WooPP.json'
 import WooRouter from '../build/WooRouter.json'
-import IERC20 from "../build/IERC20.json"
-import TestToken from "../build/TestToken.json";
+import IERC20 from '../build/IERC20.json'
+import TestToken from '../build/TestToken.json'
 
 use(solidity)
 
@@ -87,16 +87,20 @@ describe('WooRouter', () => {
       let anotherQuoteToken = await deployMockContract(owner, IERC20.abi)
       let anotherWooPP = await deployContract(owner, WooPP, [anotherQuoteToken.address, wooracle.address, ZERO_ADDR])
       await wooRouter.setPool(anotherWooPP.address)
-      await expect(wooRouter.setPool(anotherWooPP.address)).to.emit(wooRouter, 'WooPoolChanged').withArgs(anotherWooPP.address)
+      await expect(wooRouter.setPool(anotherWooPP.address))
+        .to.emit(wooRouter, 'WooPoolChanged')
+        .withArgs(anotherWooPP.address)
     })
 
     it('Prevents non-owners from setPool', async () => {
       let anotherQuoteToken = await deployMockContract(owner, IERC20.abi)
       let anotherWooPP = await deployContract(owner, WooPP, [anotherQuoteToken.address, wooracle.address, ZERO_ADDR])
-      expect(wooRouter.connect(user).setPool(anotherWooPP.address)).to.be.revertedWith('Ownable: caller is not the owner')
+      expect(wooRouter.connect(user).setPool(anotherWooPP.address)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
     })
 
-    it('setWhitelisted', async() => {
+    it('setWhitelisted', async () => {
       expect(await wooRouter.isWhitelisted(user.address)).to.eq(false)
       await wooRouter.setWhitelisted(user.address, true)
       expect(await wooRouter.isWhitelisted(user.address)).to.eq(true)
@@ -104,19 +108,23 @@ describe('WooRouter', () => {
       expect(await wooRouter.isWhitelisted(user.address)).to.eq(false)
     })
 
-    it('Prevents zero addr from setWhitelisted', async() => {
+    it('Prevents zero addr from setWhitelisted', async () => {
       expect(await wooRouter.isWhitelisted(ZERO_ADDR)).to.eq(false)
       expect(wooRouter.setWhitelisted(ZERO_ADDR, true)).to.be.revertedWith('WooRouter: target_ADDR_ZERO')
       expect(wooRouter.setWhitelisted(ZERO_ADDR, false)).to.be.revertedWith('WooRouter: target_ADDR_ZERO')
     })
 
-    it('Prevents non-owners from setWhitelisted', async() => {
+    it('Prevents non-owners from setWhitelisted', async () => {
       expect(await wooRouter.isWhitelisted(user.address)).to.eq(false)
-      expect(wooRouter.connect(user).setWhitelisted(user.address, true)).to.be.revertedWith('Ownable: caller is not the owner')
-      expect(wooRouter.connect(user).setWhitelisted(user.address, false)).to.be.revertedWith('Ownable: caller is not the owner')
+      expect(wooRouter.connect(user).setWhitelisted(user.address, true)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
+      expect(wooRouter.connect(user).setWhitelisted(user.address, false)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
     })
 
-    it('rescueFunds', async() => {
+    it('rescueFunds', async () => {
       expect(await baseToken.balanceOf(wooRouter.address)).to.eq(ZERO)
       expect(await baseToken.balanceOf(owner.address)).to.eq(ZERO)
 
@@ -129,11 +137,11 @@ describe('WooRouter', () => {
       expect(await baseToken.balanceOf(owner.address)).to.eq(mintBalance)
     })
 
-    it('Prevents zero addr as token addr from rescueFunds', async() => {
+    it('Prevents zero addr as token addr from rescueFunds', async () => {
       expect(wooRouter.rescueFunds(ZERO_ADDR, ZERO)).to.be.revertedWith('WooRouter: token_ADDR_ZERO')
     })
 
-    it('Prevents non-owners from rescueFunds', async() => {
+    it('Prevents non-owners from rescueFunds', async () => {
       expect(await baseToken.balanceOf(wooRouter.address)).to.eq(ZERO)
       expect(await baseToken.balanceOf(user.address)).to.eq(ZERO)
 
@@ -141,16 +149,18 @@ describe('WooRouter', () => {
       await baseToken.mint(wooRouter.address, mintBalance)
       expect(await baseToken.balanceOf(wooRouter.address)).to.eq(mintBalance)
 
-      expect(wooRouter.connect(user).rescueFunds(baseToken.address, mintBalance)).to.be.revertedWith('Ownable: caller is not the owner')
+      expect(wooRouter.connect(user).rescueFunds(baseToken.address, mintBalance)).to.be.revertedWith(
+        'Ownable: caller is not the owner'
+      )
       expect(await baseToken.balanceOf(user.address)).to.eq(ZERO)
     })
 
-    it('destroy', async() => {
+    it('destroy', async () => {
       await wooRouter.destroy()
       expect(wooRouter.quoteToken()).to.be.revertedWith('')
     })
 
-    it('Prevents non-owners from destroy', async() => {
+    it('Prevents non-owners from destroy', async () => {
       expect(wooRouter.connect(user).destroy()).to.be.revertedWith('Ownable: caller is not the owner')
     })
   })
@@ -180,13 +190,7 @@ describe('WooRouter', () => {
       await baseToken.mint(wooPP.address, mintBaseAmount)
 
       let minQuoteAmount = 0
-      await wooRouter.sellBase(
-        baseToken.address,
-        mintBaseAmount,
-        minQuoteAmount,
-        user.address,
-        user.address
-      )
+      await wooRouter.sellBase(baseToken.address, mintBaseAmount, minQuoteAmount, user.address, user.address)
     })
   })
 })
