190763deba7cf51a532521ac01f77a84255766d8
merlincdj <377730858@qq.com>
2021-10-18 13:23:53 +0800

    [Test]Fix bug & Add require test

--- test/Wooracle.test.ts
+++ test/Wooracle.test.ts
@@ -112,6 +112,17 @@ describe('Wooracle', () => {
     }
   })
 
+  it('postPriceList reverted with length invalid', async () => {
+    let bases = [baseToken.address, anotherBaseToken.address]
+    let newPrices = [BN_1e18, BN_2e18, BN_2e18]
+    for (let i = 0; i < bases.length; i += 1) {
+      expect(await wooracle.isValid(bases[i])).to.eq(false)
+      expect(await wooracle.price(bases[i])).to.eq(ZERO)
+    }
+
+    await expect(wooracle.postPriceList(bases, newPrices)).to.be.revertedWith('Wooracle: length_INVALID')
+  })
+
   it('postSpread', async () => {
     expect(await wooracle.spread(baseToken.address)).to.eq(ZERO)
     await wooracle.postSpread(baseToken.address, BN_1e18)
@@ -131,6 +142,16 @@ describe('Wooracle', () => {
     for (let i = 0; i < bases.length; i += 1) {
       expect(await wooracle.spread(bases[i])).to.eq(newSpreads[i])
     }
+  })
+
+  it('postSpreadList reverted with length invalid', async () => {
+    let bases = [baseToken.address, anotherBaseToken.address]
+    let newSpreads = [BN_1e18, BN_2e18, BN_2e18]
+    for (let i = 0; i < bases.length; i += 1) {
+      expect(await wooracle.spread(bases[i])).to.eq(ZERO)
+    }
+
+    await expect(wooracle.postSpreadList(bases, newSpreads)).to.be.revertedWith('Wooracle: length_INVALID')
   })
 
   it('postState', async () => {
@@ -167,6 +188,21 @@ describe('Wooracle', () => {
       expect(await wooracle.coeff(bases[i])).to.eq(newCoeffs[i])
       expect(await wooracle.isValid(bases[i])).to.eq(true)
     }
+  })
+
+  it('postStateList reverted with length invalid', async () => {
+    let bases = [baseToken.address, anotherBaseToken.address]
+    let newPrices = [BN_1e18, BN_2e18, BN_2e18]
+    let newSpreads = [BN_1e18, BN_2e18]
+    let newCoeffs = [BN_1e18, BN_2e18]
+    for (let i = 0; i < bases.length; i += 1) {
+      expect(await wooracle.price(bases[i])).to.eq(ZERO)
+      expect(await wooracle.spread(bases[i])).to.eq(ZERO)
+      expect(await wooracle.coeff(bases[i])).to.eq(ZERO)
+      expect(await wooracle.isValid(bases[i])).to.eq(false)
+    }
+
+    await expect(wooracle.postStateList(bases, newPrices, newSpreads, newCoeffs)).to.be.revertedWith('Wooracle: length_INVALID')
   })
 
   it('getPrice', async () => {
