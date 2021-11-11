4088c5e2f087c89c0d4f627a6875755c32457dd3
Lint Action <lint-action@samuelmeuli.com>
2021-11-11 16:26:13 +0000

    Fix code style issues with Prettier

--- contracts/WooStakingVault.sol
+++ contracts/WooStakingVault.sol
@@ -36,11 +36,11 @@ pragma solidity 0.6.12;
 
 import './interfaces/IWooCoolDownVault.sol';
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/Address.sol";
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
 import '@uniswap/lib/contracts/libraries/TransferHelper.sol';
 
@@ -53,15 +53,15 @@ contract WooStakingVault is ERC20, Ownable {
 
     IERC20 public stakedToken;
     IWooCoolDownVault public wooCoolDownVault;
-    mapping (address => uint256) public costSharePrice;
+    mapping(address => uint256) public costSharePrice;
 
     bool public allowContract = false;
 
     constructor(address _stakedToken, address _wooCoolDownVault)
         public
         ERC20(
-            string(abi.encodePacked("Interest bearing", ERC20(_stakedToken).name())),
-            string(abi.encodePacked("x", ERC20(_stakedToken).symbol()))
+            string(abi.encodePacked('Interest bearing', ERC20(_stakedToken).name())),
+            string(abi.encodePacked('x', ERC20(_stakedToken).symbol()))
         )
     {
         stakedToken = IERC20(_stakedToken);
@@ -71,7 +71,7 @@ contract WooStakingVault is ERC20, Ownable {
 
     /* ----- Modifier ----- */
 
-    modifier onlyHuman {
+    modifier onlyHuman() {
         if (!allowContract) {
             require(tx.origin == msg.sender);
             _;
@@ -122,10 +122,8 @@ contract WooStakingVault is ERC20, Ownable {
 
     function _updateCostSharePrice(uint256 _amount, uint256 _shares) private {
         uint256 beforeShares = balanceOf(msg.sender);
-        uint beforeCost = costSharePrice[msg.sender];
-        uint afterCost = (
-            beforeShares.mul(beforeCost).add(_amount.mul(1e18))
-        ).div(beforeShares.add(_shares));
+        uint256 beforeCost = costSharePrice[msg.sender];
+        uint256 afterCost = (beforeShares.mul(beforeCost).add(_amount.mul(1e18))).div(beforeShares.add(_shares));
 
         costSharePrice[msg.sender] = afterCost;
     }
