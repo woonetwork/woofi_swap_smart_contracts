81130a1e5c15a270232a299d80a4f64d9823428f
merlincdj <377730858@qq.com>
2022-01-13 22:54:47 +0800

    [Vault]Add StrategyBNB

---
+++ contracts/interfaces/venus/IUnitroller.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface IUnitroller {
+    function claimVenus(address holder) external;
+    function enterMarkets(address[] memory _vtokens) external;
+    function exitMarket(address _vtoken) external;
+    function getAssetsIn(address account) view external returns (address[] memory);
+    function getAccountLiquidity(address account) view external returns (uint256, uint256, uint256);
+}
