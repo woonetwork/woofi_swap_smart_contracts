c08b2a9d89724da1b0bbee2cb78a54bae1001274
merlincdj <377730858@qq.com>
2022-08-30 16:59:52 +0800

    Add StrategyAave
    Fix code style issues with Prettier
    
    fix:

---
+++ contracts/interfaces/Aave/IAavePool.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface IAavePool {
+    function deposit(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    function borrow(
+        address asset,
+        uint256 amount,
+        uint256 interestRateMode,
+        uint16 referralCode,
+        address onBehalfOf
+    ) external;
+
+    function repay(
+        address asset,
+        uint256 amount,
+        uint256 rateMode,
+        address onBehalfOf
+    ) external returns (uint256);
+
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external returns (uint256);
+
+    function getUserAccountData(address user)
+        external
+        view
+        returns (
+            uint256 totalCollateralETH,
+            uint256 totalDebtETH,
+            uint256 availableBorrowsETH,
+            uint256 currentLiquidationThreshold,
+            uint256 ltv,
+            uint256 healthFactor
+        );
+
+    function setUserEMode(uint8 categoryId) external;
+
+    function getUserEMode(address user) external view returns (uint256);
+
+    function getEModeCategoryData(uint8 categoryId)
+        external
+        view
+        returns (
+            uint16 ltv,
+            uint16 liquidationThreshold,
+            uint16 liquidationBonus,
+            address priceSource,
+            string memory label
+        );
+}
